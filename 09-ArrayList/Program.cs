4c562634e4756bc831d19c863546bd3e77a5391c
Berkant Karaca <berkantkaracatr@gmail.com>
2025-11-06 20:47:47 +0300

    09-ArrayList

---
+++ 09-ArrayList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace _09_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tip ve boyut sınırlaması yoktur. listedki özellikler çalışır ama object beklediği için kullanırken boxing unboxing yapman lazım
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(1.5);
+
+            foreach (var item in arrayList)
+            {
+                //int sonuc = item * 2; //hata verir. unboxing yapmak lazım
+
+                //tip kontrolü ile işlem yapılabilir
+                if (item.GetType() == typeof(int))
+                {
+                    int sonuc = (int)item * 2;
+                    Console.WriteLine(sonuc);
+                }
+
+                //Not: nesnelerde kullanılan tip kontrolü. 
+                //if (item as Person) 
+                //{
+                //    var result = item is Person
+                //}
+
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
