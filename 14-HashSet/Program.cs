b75d074449df2b240a99cf792d504b7de6fab286
Berkant Karaca <berkantkaracatr@gmail.com>
2025-11-06 21:01:38 +0300

    14-HashSet

---
+++ 14-HashSet/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _14_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //benzersiz elemanları tutar
+            //arama senaryolarında performanslı şekilde çalışır
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("a");
+            set.Add("a");//aynı datayı eklemeye çalıştığında hata vermez ama set içine de eklemez
+            set.Add("b");
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
