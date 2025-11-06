8823af457b060afa2c00c31ed92438a9704501fe
Berkant Karaca <berkantkaracatr@gmail.com>
2025-11-06 21:36:35 +0300

    15-Metot
    15-Metot

---
+++ 15-Metot/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace _15_Metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelamVer("Hasan");
+            int result = Topla(5, 2) * 2;
+
+            //LogMessage("Thisi is a default log");
+            LogMessage("Thisi is a default log", "Warning");
+            LogMessage(statuss: "Eror", message: "sa");
+
+            DisplayUserInfo("A");
+            DisplayUserInfo("A", 7);
+
+            Topla(5, 2);
+            Topla(5, 6, 7);
+        }
+
+        //ErişimBelirleyici Niteleyici? GeriDönüşTipi MetotAdı(parametreler?) { }
+        private static void SelamVer(string ad)
+        {
+            Console.WriteLine("Merhaba: " + ad);
+        }
+
+        static int Topla1(int n, int m) //default erişim belirteci metotlarda private'dir
+        {
+            try
+            {
+                return n + m;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw; //her koşulda ya bir return yada exception fırlatman lazım
+            }
+        }
+
+        public static void LogMessage(string message, string logLevel = "INFO") //default değeri atanan parametreler en sona yazılır
+        {
+            Console.WriteLine($"{logLevel} : {message}");
+        }
+
+        public static void LogMessage(string message, string logLevel = "INFO", string statuss = "1")
+        {
+            Console.WriteLine($"{logLevel} : {message}, {statuss}");
+        }
+
+        //null geçilebilen parametre
+        public static void DisplayUserInfo(string name, int? age = null)
+        {
+            if (age.HasValue) // age is null
+                Console.WriteLine($"{name} is {age} years old");
+            else
+                Console.WriteLine($"{name} did not provide an age");
+        }
+
+        #region Metot Overloading
+        //Bir metodun birden fazla kullanım şekli vardır. metot imzası değişir. metot imzası da sadece parametrelerdir
+        static int Topla(int n, int m)
+        {
+            return n + m;
+        }
+
+        static int Topla(int n, int m, int l)
+        {
+            return n + m + l;
+        }
+
+        static int Topla(int n, decimal m)
+        {
+            return n + (int)m;
+        }
+        #endregion
+    }
+}
---
+++ 15-Metot/Program2.cs
@@ -0,0 +1,116 @@
+﻿namespace _15_Metot
+{
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            #region Params
+            int[] nums = { 1, 2, 3 };
+
+            Console.WriteLine(Topla1(nums)); //bizi dizi olması sınırlandırdı. params bunu esnekleştirir.
+            Console.WriteLine(Topla2(1, 2, 3, 4)); //istediğin kadar parametre geçebilirsin
+            #endregion
+
+            #region Out
+            //parametre üzerinden dışarıya data çıkarır. çıkış paramertesidir. içerden dışarıya data gönderir
+            double sonuc, bolumdenKalan;
+            sonuc = Bolme(15, 2, out bolumdenKalan);
+            Console.WriteLine($"15/2: {sonuc}, Kalan: {bolumdenKalan}");
+            #endregion
+
+            #region Ref
+            int a = 10, b = 12;
+
+            Console.WriteLine($"İşlem öncesi a: {a}, b: {b}");
+            Console.WriteLine();
+
+            ToplamDeger(a);
+            FarkDeger(ref b);
+            Console.WriteLine($"İşlem Sonrası a: {a}, b: {b}");
+
+
+            int ToplamDeger(int a)//metot içinde yazılan local metotlar
+            {
+                return a += 10;
+            }
+
+            void FarkDeger(ref int b)//metot içinde yazılan local metotlar
+            {
+                b -= 5;
+            }
+
+            #endregion
+
+            #region ref2
+            int sayi = 5;
+            Console.WriteLine(sayi);
+
+            DegeriDegistir(ref sayi);
+            Console.WriteLine(sayi);
+            #endregion
+
+            #region Local Metotlar-Fonksiyonlar
+            //metot içinde metot tanımlama. public ve static kullanılmaz. yazıldığı fonksiyonunkileri alır. bu metotlara dışarıdan erişilemez. public olsa bile
+            int Hesapla(int a)
+            {
+                return a + 10;
+            }
+            #endregion
+        }
+
+        //metotları yazarken info veren yapı: Topla1(); üstüne mouse getir vs
+        /// <summary>
+        /// N sayıda değişkeni toplayan metot
+        /// </summary>
+        /// <param name="sayilar">Birden fazla sayı girişi</param>
+        /// <returns>Sayıların toplam değeri</returns>
+        public static int Topla1(int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+
+            return toplam;
+        }
+
+        public static int Topla2(params int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+
+            return toplam;
+        }
+
+        /// <summary>
+        /// Bölme işlemini gerçekleştirir
+        /// </summary>
+        /// <param name="bolunen">Bölünecek Sayı</param>
+        /// <param name="bolen">Bölen Sayı</param>
+        /// <param name="kalan">Kalan Sayı</param>
+        /// <returns>Sonuç</returns>
+        public static double Bolme(double bolunen, double bolen, out double kalan)
+        {
+            kalan = bolunen % bolen;
+            return bolunen / bolen;
+        }
+
+        public static void DegeriDegistir(ref int x)
+        {
+            x = x + 10;
+        }
+
+        #region BestPractice
+        /*  Adlandırma Kuralları
+         *  1- Anlamlı isimler: Calculate yerine CalculateTotalPrice
+         *  2- Fiil kullanımı: GetCustomer, SaveFile, SendEmail...
+         *  3- camelCase ve PascalCase: metotlar PascalCase, değişkenler camelCase
+         *  SOLID
+        */
+        #endregion
+    }
+}
