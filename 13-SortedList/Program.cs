3af5575a0c49dcb14124d5f50a7f3f5757697d4f
Berkant Karaca <berkantkaracatr@gmail.com>
2025-11-06 21:00:32 +0300

    13-SortedList

---
+++ 13-SortedList/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _13_SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //key-value çiftlerine göre sıralı bir şekilde data saklar.
+            SortedList<int, string> sinif = new SortedList<int, string>();
+            sinif.Add(3, "a"); //tryadd var
+            sinif.Add(1, "b");
+            sinif.Add(2, "c");
+            //sinif.Add(2, "csda"); //aynı keye air başka kayıt eklenemez. hata verir.
+            sinif.Add(5, "d");
+
+            sinif.Remove(5);
+
+            foreach (var item in sinif)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+    }
+}
