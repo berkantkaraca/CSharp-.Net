6e317133c6af8e440be017113f4bcf52244ac2ef
Berkant Karaca <berkantkaracatr@gmail.com>
2025-11-14 22:04:10 +0300

    44-IoC-DependencyInversion

---
+++ 44-IoC-DependencyInversion/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _44_IoC_DependencyInversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soyutlara manuel bağlı kalmaktır.
+            //Sınıflar doğrudan birbirine değil, Soyutlamalara (interface’e) bağımlı olurlar.
+            //35-AdoNet örneğindeki StudentRepoyu daha sonra StudentEntityRepoya taşıdık. Bunun kolaylıkla geçiş yapmasını sağlayan Dependency Inversion kavramıdır. Soyut sınıflara bağlı olduğumuz için kolaylaştı. IStudent repo = new StudentRepo();
+
+            //Loose Coupling olur
+        }
+    }
+}
